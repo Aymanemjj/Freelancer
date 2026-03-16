858e049c4a5b359478f2b04ead2065f1b6fd3b9e
Aymane <aymanemj@proton.me>
2026-03-16 14:37:14 +0000

    General exceptions handeling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,49 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ModelNotFoundException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ressource introuvable',
+            ], 404);
+        });
+
+        $exceptions->render(function (MethodNotAllowedHttpException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Méthod HTTP non autorisé',
+            ], 405);
+        });
+
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Non authentifié',
+            ], 401);
+        });
+
+        $exceptions->render(function (\Exception $e, $request) {
+            return response()->json([
+                'success' => false,
+                'message' => 'error intern de serveur',
+                'exception' => "Une erreur interne est survenue. Veuillez réessayer.",
+                "error" => $e->getMessage()
+            ], 500);
+        });
     })->create();
