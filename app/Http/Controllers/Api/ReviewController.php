85f66db5ef63c5add514e1bc2c30378785370e87
Aymane <aymanemj@proton.me>
2026-03-30 01:52:55 +0100

    UpdateReviewRequest, and finished Review controller

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreReviewRequest;
+use App\Http\Requests\UpdateReviewRequest;
 use App\Services\ReviewService;
+use Exception;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -16,9 +19,53 @@ class ReviewController extends Controller
     }
 
 
-    public function index($id){
-        return $this->ReviwService->index($id);
+    public function index($id)
+    {
+        try {
+            return $this->ReviwService->index($id);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
     }
 
-    public function store($id, )
+    public function store($id, StoreReviewRequest $request)
+    {
+        try {
+            return $this->ReviwService->store($id, $request);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function show() {}
+
+    public function update($id, UpdateReviewRequest $request)
+    {
+        try {
+            return $this->ReviwService->update($id, $request);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            return $this->ReviwService->destroy($id);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'fail',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
---
+++ app/Http/Requests/UpdateReviewRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'rating' => 'min:0|max;5|numeric',
+            "comment" => 'string'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+        $response = response()->json([
+            'message' => 'Invalid data send',
+            'details' => $errors->messages(),
+        ], 422);
+
+        throw new HttpResponseException($response);
+    }
+}
